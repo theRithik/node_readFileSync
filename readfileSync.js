b360361b4d7787667367cbdff0cebf6aae9890f7
rithik <115656594+theRithik@users.noreply.github.com>
2023-03-23 15:18:07 +0530

    Update readfileSync.js

--- readfileSync.js
+++ readfileSync.js
@@ -1,14 +1,6 @@
 const fs = require('fs')
 console.log('started')
-// fs.readFile('text.txt', 'utf8', (err, data)=>{
-//     if(err){
-//         console.log(err)
-//     }
-//     else{
-//         console.log(data.toString)
-//     }
 
-// })
 try{
     const data = fs.readFileSync('text.txt')
     console.log('reading the file'),
