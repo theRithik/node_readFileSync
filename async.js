eb5bd18555e058cc805981b2e2c74e98b81cc712
rithik <115656594+theRithik@users.noreply.github.com>
2023-03-24 15:11:38 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,8 @@
+console.log('start')
+
+setTimeout(()=>{console.log('1')},5000)
+setTimeout(()=>{console.log('2')},0)
+setTimeout(()=>{console.log('3')},2000)
+setTimeout(()=>{console.log('4')},0)
+
+console.log('end')
